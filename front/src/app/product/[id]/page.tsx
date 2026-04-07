2382d020046336be3475b2c3028cbdca8d698083
Alejandro-Mirena <alejandromirena.13@gmail.com>
2026-04-06 21:28:41 -0500

    Adding new images, and changing redirect links to product view

--- front/src/app/product/[id]/page.tsx
+++ front/src/app/product/[id]/page.tsx
@@ -1,43 +1,51 @@
-"use client"
+"use client";
 
-import { productsToPreLoad } from "@/libraries/mockedupProducts"
-import { useState, use } from "react"
-import Link from "next/link"
+import { productsToPreLoad } from "@/libraries/mockedupProducts";
+import { useState, use } from "react";
+import Link from "next/link";
 
 const ProductDetailPage = ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = use(params);
 
-  const { id } = use(params)
+  const product = productsToPreLoad.find((p) => p.id === Number(id));
 
-  const product = productsToPreLoad.find(
-    (p) => p.id === Number(id)
-  )
-
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
   if (!product) {
     return (
       <div className="text-center py-20">
-        <h1 className="text-[#1D1D1F] text-2xl font-semibold mb-4">Producto no encontrado</h1>
-        <Link href="/" className="text-[#0071E3] text-sm hover:underline bg-[#0071E3] hover:bg-[#4992db] transition-colors text-white px-6 py-3 rounded-lg text-sm font-medium">Volver al inicio</Link>
+        <h1 className="text-[#1D1D1F] text-2xl font-semibold mb-4">
+          Producto no encontrado 😓
+        </h1>
+        <Link
+          href="/products"
+          className=" text-sm hover:cursor-wait bg-[#0071E3] hover:bg-[#0077ed] transition-colors text-white px-6 py-3 rounded-lg  font-medium"
+        >
+          Volver a productos
+        </Link>
       </div>
-    )
+    );
   }
 
   return (
     <div className="px-8 py-12 max-w-5xl mx-auto">
-
-      <Link href="/" className="text-[#0071E3] text-sm hover:underline mb-8 inline-block">
+      <Link
+        href="/products"
+        className="text-[#0071E3] text-sm hover:underline mb-8 inline-block"
+      >
         ← Volver
       </Link>
 
       <div className="grid grid-cols-2 gap-12">
-
         <div className="bg-[#F5F5F7] rounded-2xl flex items-center justify-center p-10 h-96">
-          <img src={product.image} alt={product.name} className="h-full object-contain" />
+          <img
+            src={product.image}
+            alt={product.name}
+            className="h-full object-contain"
+          />
         </div>
 
         <div className="flex flex-col justify-center">
-
           <h1 className="text-[#1D1D1F] text-3xl font-semibold tracking-tight mb-3">
             {product.name}
           </h1>
@@ -52,11 +60,15 @@ const ProductDetailPage = ({ params }: { params: Promise<{ id: string }> }) => {
 
           <p className="text-xs text-[#6E6E73] mb-4">
             Stock disponible:
-            <span className="text-[#34C759] font-medium ml-1">{product.stock} unidades</span>
+            <span className="text-[#34C759] font-medium ml-1">
+              {product.stock} unidades
+            </span>
           </p>
 
           <div className="flex items-center gap-4 mb-6">
-            <span className="text-[#1D1D1F] text-sm font-medium">Cantidad:</span>
+            <span className="text-[#1D1D1F] text-sm font-medium">
+              Cantidad:
+            </span>
             <div className="flex items-center border border-[#E8E8ED] rounded-lg overflow-hidden">
               <button
                 onClick={() => setQuantity((q) => Math.max(1, q - 1))}
@@ -68,7 +80,9 @@ const ProductDetailPage = ({ params }: { params: Promise<{ id: string }> }) => {
                 {quantity}
               </span>
               <button
-                onClick={() => setQuantity((q) => Math.min(product.stock, q + 1))}
+                onClick={() =>
+                  setQuantity((q) => Math.min(product.stock, q + 1))
+                }
                 className="px-4 py-2 text-[#1D1D1F] hover:bg-[#F5F5F7] transition-colors text-lg"
               >
                 +
@@ -79,11 +93,10 @@ const ProductDetailPage = ({ params }: { params: Promise<{ id: string }> }) => {
           <button className="bg-[#0071E3] hover:bg-[#0077ED] transition-colors text-white py-3 rounded-xl text-sm font-medium cursor-pointer ">
             Agregar al carrito — ${(product.price * quantity).toLocaleString()}
           </button>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailPage
+export default ProductDetailPage;
--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,6 +1,9 @@
 import ProductsPage from "@/views/ProductsPage";
 
 const page = () => {
+  // const productsRsponse = getProductsByCategory(1); //todos los productos que tengan categoria 1
+
+  // return <ProductsPage title=" - Nuestros Productos" products={productsRsponse} />;
   return <ProductsPage />;
 };
 
